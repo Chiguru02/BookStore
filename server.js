2e68ded9ac3665bba9743ac15af126f2b405e03f
Chandrasekar Chigurupati <cchigurupati5@gmail.com>
2024-06-22 18:22:05 +0530

    intial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+const expressLayouts = require("express-ejs-layouts");
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("mongoose connection successful"));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    "Server is running on port: " +
+      process.env.PORT +
+      "\n" +
+      process.env.DATABASE_URL
+  );
+});
